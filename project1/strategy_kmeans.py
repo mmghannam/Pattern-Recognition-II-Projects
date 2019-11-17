bd96656b18765b6726a4811ef2d974e1762f6d3d
Mahmoud Hashem <mahmoud.gamal30@yahoo.com>
2019-11-17 15:34:12 +0100

    updates task 1.5

--- project1/strategy_kmeans.py
+++ project1/strategy_kmeans.py
@@ -1,4 +1,10 @@
+from sklearn.cluster import KMeans
+from scipy.spatial import distance_matrix
 
-
-def strategy_kmeans():
-    pass
+def strategy_kmeans(data, k=3, seed=1):
+    kmeans = KMeans(n_clusters=k, random_state=seed)
+    kmeans.fit(data)
+    kmeans.labels_
+    centers = kmeans.cluster_centers_
+    print(centers)
+    print(distance_matrix(centers,centers))
--- project1/task1.5.py
+++ project1/task1.5.py
@@ -1,4 +1,4 @@
 import project1.strategy_kmeans as sk
-
+import numpy as np
 if __name__ == '__main__':
-    sk.strategy_kmeans()
+    sk.strategy_kmeans( np.array([[1, 2], [1, 4], [1, 0],[10, 2], [10, 4], [10, 0]]))
