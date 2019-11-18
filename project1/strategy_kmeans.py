adbc42a7feee8ca17a5f2e97014945f8b05fa3cb
Mahmoud Hashem <mahmoud.gamal30@yahoo.com>
2019-11-18 18:18:24 +0100

    implements kmeans method

--- project1/strategy_kmeans.py
+++ project1/strategy_kmeans.py
@@ -1,10 +1,22 @@
 from sklearn.cluster import KMeans
 from scipy.spatial import distance_matrix
+import numpy as np
+
 
 def strategy_kmeans(data, k=3, seed=1):
     kmeans = KMeans(n_clusters=k, random_state=seed)
     kmeans.fit(data)
-    kmeans.labels_
     centers = kmeans.cluster_centers_
-    print(centers)
-    print(distance_matrix(centers,centers))
+    avg_largest_distance = distance_matrix(centers, centers).sum()
+
+    solutions = centers.copy()
+    for i in range(k):
+        for item in data[np.argwhere(kmeans.labels_ == i)]:
+            centers[i] = item
+            suggested_distance = distance_matrix(centers, centers).sum()
+            if avg_largest_distance < suggested_distance:
+                avg_largest_distance = avg_largest_distance
+                solutions[i] = item
+
+    print(solutions)
+    return solutions
--- project1/task1.5.py
+++ project1/task1.5.py
@@ -1,4 +1,5 @@
 import project1.strategy_kmeans as sk
 import numpy as np
 if __name__ == '__main__':
-    sk.strategy_kmeans( np.array([[1, 2], [1, 4], [1, 0],[10, 2], [10, 4], [10, 0]]))
+    data =  np.array([[1, 2], [1, 4], [1, 0],[10, 2], [10, 4], [10, 0]])
+    sk.strategy_kmeans(data)
