b580af07ac44dde54e2359a71d866b8ba437871f
Mahmoud Hashem <mahmoud.gamal30@yahoo.com>
2019-11-23 19:32:39 +0100

    fixs bugs in kmeans

--- project1/strategy_kmeans.py
+++ project1/strategy_kmeans.py
@@ -1,21 +1,32 @@
 from sklearn.cluster import KMeans
 from scipy.spatial import distance_matrix
 import numpy as np
+from itertools import permutations
+import matplotlib.pyplot as plt
 
 
 def strategy_kmeans(data, k=3, seed=1):
     kmeans = KMeans(n_clusters=k, random_state=seed)
     kmeans.fit(data)
     centers = kmeans.cluster_centers_
-    avg_largest_distance = distance_matrix(centers, centers).sum()
+    avg_largest_distance = round(distance_matrix(centers, centers).sum())
 
-    solutions = centers.copy()
-    for i in range(k):
-        for item in data[np.argwhere(kmeans.labels_ == i)]:
-            centers[i] = item
-            suggested_distance = distance_matrix(centers, centers).sum()
-            if avg_largest_distance < suggested_distance:
-                avg_largest_distance = avg_largest_distance
-                solutions[i] = item
+    solution = centers.copy()
+    perms = list(permutations(np.arange(k)))
 
+    solutions = (np.repeat([solution], len(perms), axis=0))
+    distances = []
+    for i, perm in enumerate(perms):
+        largest_distance = avg_largest_distance
+        for center in perm:
+            centers = solutions[i].copy()
+            for item in data[np.argwhere(kmeans.labels_ == center)]:
+                centers[center] = item
+                suggested_distance = round(distance_matrix(centers, centers).sum())
+                if largest_distance < suggested_distance:
+                    largest_distance = suggested_distance
+                    solutions[i][center] = item
+        distances.append(distance_matrix(solutions[i], solutions[i]).sum())
+    distances = np.array(distances)
+    solutions = solutions[np.argmax(distances)]
     return solutions
--- project1/task1.5.py
+++ project1/task1.5.py
@@ -2,11 +2,13 @@ import project1.strategy_kmeans as sk
 import project1.Data as d
 import numpy as np
 from scipy.spatial import distance_matrix
+import timeit
 
 if __name__ == '__main__':
     data = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
-    truth, data = d.Data(k=3, dim=3, n=1000)
-    predictions = (sk.strategy_kmeans(data))
+
+    k = 4
+    truth, data = d.Data(k=k, dim=2, n=1000)
+    predictions = (sk.strategy_kmeans(data, k=k))
     print(distance_matrix(predictions, predictions).sum())
     print(distance_matrix(truth, truth).sum())
-
