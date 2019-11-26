c88eafbed429b729df1a11a46a3256c79f354f5d
Sarah0711 <sarahjmi07@gmail.com>
2019-11-26 19:57:36 +0100

    1.Changes with respect to facematrix data 2.Dimensionality reduction with PCA then application of convex hull strategy 3.Changed distancematrix to cdist to consider euclidean distance and for error manhattan distance 4.Visualisation of extracted farthest data 5.Evaluate function to evaluate strategies with own data and given data with different variations

--- project1/strategy_convex_hull.py
+++ project1/strategy_convex_hull.py
@@ -2,11 +2,12 @@ from scipy.spatial import ConvexHull
 from scipy.spatial import distance_matrix
 import numpy as np
 from itertools import combinations
+from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
-
+from scipy.spatial import distance
 
 def strategy_convex_hull(data, k=4, seed=1):
-    hull = ConvexHull(data)
+    hull =ConvexHull(data)
     solution = []
     vertices = hull.vertices
     while len(solution) + len(vertices) <= k:
@@ -20,18 +21,21 @@ def strategy_convex_hull(data, k=4, seed=1):
         hull = ConvexHull(data)
         vertices = hull.vertices
     largest_distance = 0.0
-    for case in list(combinations(vertices, k - len(solution))):
-        if len(solution) == 0:
-            temp = data[list(case)]
-        else:
-            temp = np.concatenate((np.array(solution), data[list(case)]), axis=0)
-        suggested_distance = round(distance_matrix(temp, temp).sum())
-        print(temp)
-        print(suggested_distance)
-        if largest_distance < suggested_distance:
-            largest_distance = suggested_distance
-            best_addition = temp
-    solution = best_addition
+    max_distance = 0.0
+    temp_index = vertices[0]
+    temp = data[temp_index]
+    solution.append(temp_index)
+    vertices = vertices[vertices!=temp_index]
+    while(len(solution)<k):
+        max_distance = 0.0
+        for j in vertices:
+            suggested_distance = round(distance.euclidean(temp, data[j]))
+            if max_distance < suggested_distance:
+                max_distance = suggested_distance
+                best_addition = j
+        solution.append(best_addition)
+        temp = data[best_addition]
+        vertices = vertices[vertices!=best_addition]
     return np.array(solution)
 
 
--- project1/strategy_kmeans.py
+++ project1/strategy_kmeans.py
@@ -3,7 +3,7 @@ from scipy.spatial import distance_matrix
 import numpy as np
 from itertools import permutations
 import matplotlib.pyplot as plt
-
+from scipy.spatial import distance
 
 def strategy_kmeans(data, k=3, seed=1):
     kmeans = KMeans(n_clusters=k, random_state=seed)
@@ -12,7 +12,7 @@ def strategy_kmeans(data, k=3, seed=1):
     avg_largest_distance = round(distance_matrix(centers, centers).sum())
 
     solution = centers.copy()
-    perms = list(permutations(np.arange(k)))
+    perms = [np.array(np.arange(k))]
 
     solutions = (np.repeat([solution], len(perms), axis=0))
     distances = []
@@ -22,11 +22,11 @@ def strategy_kmeans(data, k=3, seed=1):
             centers = solutions[i].copy()
             for item in data[np.argwhere(kmeans.labels_ == center)]:
                 centers[center] = item
-                suggested_distance = round(distance_matrix(centers, centers).sum())
+                suggested_distance = round(distance.cdist(centers, centers,'cityblock').sum())
                 if largest_distance < suggested_distance:
                     largest_distance = suggested_distance
                     solutions[i][center] = item
-        distances.append(distance_matrix(solutions[i], solutions[i]).sum())
+        distances.append(distance.cdist(solutions[i], solutions[i],'cityblock').sum())
     distances = np.array(distances)
     solutions = solutions[np.argmax(distances)]
     return solutions
