a08406878bd498cf31968a7a4d1a151c48b8c45b
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2019-12-12 13:03:12 +0100

    Add Hopfiled class structure

---
+++ project2/task2_1/hopfield.py
@@ -0,0 +1,19 @@
+class Hopfield:
+    def __init__(self, number_of_neurons):
+        self.weight_matrix = self.initialize_weight_matrix()
+        self.state = self.initialize_state()
+
+    def initialize_state(self):
+        return
+
+    def initialize_weight_matrix(self):
+        pass
+
+    def update(self):
+        pass
+
+    def runSynchronous(self, synchronous=False):
+        pass
+
+    def energy(self):
+        pass
