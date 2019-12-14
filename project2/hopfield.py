2f0648eca1a3b83417f3dd983d014d4062e5c628
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2019-12-14 14:28:52 +0100

    Refactoring

--- project2/hopfield.py
+++ project2/hopfield.py
@@ -2,26 +2,30 @@ import numpy as np
 
 
 class Hopfield:
-    def __init__(self, number_of_neurons, seed=100, convergence_params=[]):
+    def __init__(self, number_of_neurons, seed=100):
         np.random.seed(seed)
         self.number_of_neurons = number_of_neurons
-        self.weight_matrix = self.initialize_weight_matrix()
-        self.state = self.initialize_state()
-        self.thresholds = self.initialize_thresholds()
-        self.previous_energies = self.initialize_previous_energies()
-        self.convergence_params = convergence_params
+        self.initialize()
+
+    def initialize(self):
+        self.initialize_state()
+        self.initialize_thresholds()
+        self.initialize_weight_matrix()
+        self.initialize_previous_energies()
+
+    reset = initialize  # alias for initialize method
 
     def initialize_previous_energies(self):
-        return [np.NINF]
+        self.previous_energies = [np.NINF, self.energy()]
 
     def initialize_state(self):
-        return self.__get_random_polar_state()
+        self.state = self.__get_random_polar_state()
 
     def initialize_thresholds(self):
-        return np.zeros(self.number_of_neurons)
+        self.thresholds = np.zeros(self.number_of_neurons)
 
     def initialize_weight_matrix(self):
-        return np.ones(2 * [self.number_of_neurons])
+        self.weight_matrix = np.ones(2 * [self.number_of_neurons])
 
     def update(self, synchronous=False):
         if synchronous:
@@ -34,37 +38,33 @@ class Hopfield:
     def asynchronous_choice(self):
         return np.random.randint(self.number_of_neurons)
 
-    def run(self, synchronous=False):
-        while not self.is_done():
+    def run(self, synchronous=False, convergence_params=[]):
+        while not self.is_done(*convergence_params):
             self.update(synchronous)
-            self.previous_energies.append(self.energy())
 
     def energy(self):
         return -0.5 * self.state @ self.weight_matrix @ self.state + self.thresholds @ self.state
 
-    def is_done(self):
-        tolerance = self.convergence_params[0]
-        return self.previous_energies == [np.NINF] or \
-               np.isclose(self.previous_energies[-1], self.previous_energies[-2], rtol=tolerance)
+    def is_done(self, tolerance):
+        return np.isclose(self.previous_energies[-1], self.previous_energies[-2], rtol=tolerance)
 
-    def multiple_runs(self, n):
+    def multiple_runs(self, n, synchronous=False, convergence_params=[]):
         lowest_energies = [np.Inf]
-        for i in range(n):
-            self.run()
+        for _ in range(n):
+            self.run(synchronous, convergence_params)
             if self.previous_energies[-1] < lowest_energies[-1]:
                 best_state = self.state.copy()
-                print(best_state)
                 lowest_energies = self.previous_energies.copy()
-            self.initialize_state()
-            self.initialize_previous_energies()
-        self.state = best_state
+                self.reset()
         self.previous_energies = lowest_energies
+        self.state = best_state
 
+        return self
 
     def __get_random_polar_state(self):
         return 2 * np.random.randint(2, size=self.number_of_neurons) - 1
 
 
 if __name__ == '__main__':
-    x = Hopfield(10, convergence_params=[0.1]) 
-    x.multiple_runs(3)
+    x = Hopfield(5)
+    print(x.multiple_runs(3, convergence_params=[0.1]).state)
