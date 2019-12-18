1e9f6c4c0a8b4079da0dbc5eb97b2c40bf944775
Mahmoud Hashem <mahmoud.gamal30@yahoo.com>
2019-12-18 22:07:29 +0100

    reformat and plotting

--- project2/task2_1/kRooksHopfield.py
+++ project2/task2_1/kRooksHopfield.py
@@ -1,13 +1,12 @@
-import functools
-
 from project2.hopfield import Hopfield
 import numpy as np
-import timeit
+import matplotlib.pyplot as plt
 
 
 class kRooksHopfield(Hopfield):
     def __init__(self, k, seed=100):
         self.k = k
+        self.best_iterations = np.Inf
         super().__init__(k * k, seed)
 
     def initialize_weight_matrix(self):
@@ -28,7 +27,8 @@ class kRooksHopfield(Hopfield):
             zeros_after_J = np.tile(zeros, self.k - diagonal_index - 1)
             column = np.concatenate((zeros_before_J, J, zeros_after_J), axis=1)
             J_diagonal_matrix.append(column)
-        J_diagonal_matrix = np.array(J_diagonal_matrix).reshape(self.number_of_neurons, self.number_of_neurons)
+        J_diagonal_matrix = np.array(J_diagonal_matrix).reshape(self.number_of_neurons,
+                                                                self.number_of_neurons)
 
         return -2 * J_diagonal_matrix
 
@@ -57,6 +57,8 @@ class kRooksHopfield(Hopfield):
         self.max_iterations = convergence_params[0]
         self.iterations = 0
         super().run(synchronous, convergence_params)
+        if self.iterations < self.best_iterations:
+            self.best_iterations = self.iterations
 
     def is_done(self, tolerance):
         self.iterations += 1
@@ -74,6 +76,38 @@ class kRooksHopfield(Hopfield):
 
 if __name__ == '__main__':
     for k in [4, 8]:
-        x = kRooksHopfield(k)
-        x.multiple_runs(n=5, convergence_params=[100])
-        print(x.reshaped_state())
+        ns = [1, 10, 100]
+        criterias = [10, 100, 1000]
+        fig, axes = plt.subplots(nrows=3, ncols=3)
+        fig.suptitle("K-Rooks K: " + str(k), fontsize=14)
+        fig.subplots_adjust(wspace=1.0, hspace=1.0)
+
+        i = -1
+        for n in ns:
+
+            for criteria in criterias:
+                x = kRooksHopfield(k)
+
+                x.multiple_runs(n=n, convergence_params=[criteria])
+                print("K: {}\n iter: {}\n state:\n {}\n energy: {}, solved: {}\n".format(str(k), str(x.best_iterations),
+                                                                                         str(x.reshaped_state()),
+                                                                                         x.previous_energies[-1],
+                                                                                         x.solved()))
+
+                i += 1
+                col = i % 3
+                row = int(i / 3)
+                img = x.reshaped_state()
+                title = "R:" + str(n) + " I: " + str(x.best_iterations) + "/" + str(criteria) + ' G: ' + (
+                    'T' if x.solved() else 'F')
+                axes[row, col].matshow(img, cmap='gray')
+                axes[row, col].set_title(title, pad=20)
+
+        fig.subplots_adjust(top=0.85)
+        fig.set_size_inches(6, 6)
+        savtitle = "KRooks with K:" + str(k)
+        plt.savefig(savtitle + ".jpg",
+                    format='jpeg',
+                    transparent=False,
+                    bbox_inches='tight', pad_inches=0.01)
+        plt.close()
